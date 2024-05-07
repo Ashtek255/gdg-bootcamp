1cfeaa2dded5b3531248577b9138cd3ba34dd8f9
Ashtek255 <ashlizerhussein8@gmail.com>
2024-05-07 20:53:20 +0300

    statement added

--- main.dart
+++ main.dart
@@ -3,7 +3,8 @@ main() {
   const num1 = 13;
   const num2 = 12;
 
-  var sum =(num1 * num2) + num1 + num2;
+  var sum = (num1 * num2) + num1 + num2;
 
   print(sum);
+  print("you have successfully added a number");
 }
